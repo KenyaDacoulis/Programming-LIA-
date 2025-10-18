7e72d8ca8520e42039e225dc7667c6dee6206ab2
Ava <Avabinetti.18@gmail.com>
2025-10-17 21:38:44 -0400

    Update test.py

--- test.py
+++ test.py
@@ -5,22 +5,22 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
 
 data = pd.read_csv("animal-data-1.csv", nrows= 100)
 
 
-# bar graph showing number of animals per species for first 100 rows of data               
-x = data['speciesname'].value_counts().index
-y = data['speciesname'].value_counts().values
+#pie chart showing the most common intake reasons for the first 100 rows of data
+#x: reasons( abandoned ,moving etc.) y: count(as a %)
 
-plt.bar(x,y, color = "cornflowerblue")
-plt.xlabel("Species")
-plt.ylabel("Count")
-plt.title("Number of Animals by Species")
+labels = data['intakereason'].value_counts().index   
+size = data['intakereason'].value_counts().values  
+
+plt.pie (size,autopct='%1.1f%%')
+plt.title("Animals Intake Reason")
+plt.legend(labels=labels) #legend so that the sections are clearly defined 
 plt.show()
-
-
 
